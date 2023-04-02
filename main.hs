e557efa6ab7ee27dd9b70e64b59e0817777fa133
ren <rendemare@gmail.com>
2023-04-02 23:51:42 +0300

    deleted redundant file

--- main.hs
+++
@@ -1,5 +0,0 @@
--- double factorial : if x = 6 then x = 2 * 4 * 6 else if x = 7 then x = 1 * 3 * 5 * 7
-doubleFact :: Integer -> Integer
-doubleFact 0 = 1
-doubleFact 1 = 1
-doubleFact x = x * doubleFact (x - 2)
