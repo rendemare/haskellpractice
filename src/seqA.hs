fdb223bd27ac009e5bb8f3018a2ef355511a033f
ren <rendemare@gmail.com>
2023-04-03 18:33:54 +0300

    fixme

--- src/seqA.hs
+++ src/seqA.hs
@@ -1,4 +1,5 @@
 -- a0 = 1, a1 = 2, a2 = 3, a(k+3) = a(k+2) + a(k+1) - 2 * a(k)
+-- FIXME: неэффективное решение, необходимо переписать с помощью аккумулятора.
 seqA :: Integer -> Integer
 seqA x | x < 3 = x + 1
        | otherwise = seqA(x - 1) + seqA(x - 2) - 2 * seqA(x - 3)
